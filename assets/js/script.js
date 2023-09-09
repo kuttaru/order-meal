d60618c84b5f75159dac4c3c662dea048bfb5741
Arun Kumar Vr <arun.vr@electronikmedia.com>
2023-09-09 18:53:24 +0530

    signin

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,20 @@
   $(document).ready(function() {
 
 
-$(document.body).on('click', '.links_wrap h3' ,function(){
-  if ($(this).parent('.links_wrap').hasClass('show')) {
-    $(this).parent('.links_wrap').removeClass("show");
-  } else {
-    $(this).parent('.links_wrap').addClass("show");
-  }
-});
+  $('body a').each(function(){ 
+    var oldUrl = $(this).attr("href"); 
+    var newUrl = ("/order-meal" + oldUrl );
+    $(this).attr("href", newUrl);
+  });
+
+
+  $(document.body).on('click', '.links_wrap h3' ,function(){
+    if ($(this).parent('.links_wrap').hasClass('show')) {
+      $(this).parent('.links_wrap').removeClass("show");
+    } else {
+      $(this).parent('.links_wrap').addClass("show");
+    }
+  });
     
 
 
