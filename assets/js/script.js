23f9ad02c0c68f407cb7e32e1228a4ec5d0b8d1e
Arun Kumar Vr <arun.vr@electronikmedia.com>
2023-09-09 15:01:34 +0530

    landing-page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,16 @@
 (function($) {
   $(document).ready(function() {
 
-	$('.sidenav').sidenav({
-	  edge: 'right', 
-	});
-    // $('.modal').modal();
 
-    setTimeout(function(){
-        $("#main-video").trigger('play');
-
-    }, 5000);
-
-
+$(document.body).on('click', '.links_wrap h3' ,function(){
+  if ($(this).parent('.links_wrap').hasClass('show')) {
+    $(this).parent('.links_wrap').removeClass("show");
+  } else {
+    $(this).parent('.links_wrap').addClass("show");
+  }
+});
     
 
-    $('.modal').modal({
-        dismissible: true,
-        onOpenEnd: function(modal, trigger) {
-        $("#video").trigger('play'); 
-        },
-        onCloseEnd: function() { 
-        $("#video").trigger('pause');
-        } 
-    });
-
-
-
-  $('.collapsible').collapsible();
-
-  $('.single-slider').owlCarousel({
-      loop:true,
-      margin:10,
-      nav:true,
-      items:1,
-  })
-
-
-$('.three-item').owlCarousel({
-    loop:true,
-    margin:30,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:2
-        }
-    }
-})
-
-
-
-  $(".close-btn").click(function(){
-    $(".accc-content").slideUp();
-  });
-
-
-  $(".acc-item").click(function(){
-    $(this).addClass('active');
-    $(this).find(".accc-content").slideDown();
-  });
 
 
 
